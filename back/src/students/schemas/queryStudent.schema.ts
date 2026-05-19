f7c37d551341dc510dc1f99f6bcd83abe417763f
flchris <filetelopezchristian@gmail.com>
2026-05-19 11:28:47 -0600

    refactor: delete the optional filters

--- back/src/students/schemas/queryStudent.schema.ts
+++ back/src/students/schemas/queryStudent.schema.ts
@@ -5,8 +5,6 @@ import { z } from "zod";
 export const StudentFiltersSchema = z.object({
   guardianId: numberFilterSchema().optional(),
   studentStatusId: numberFilterSchema().optional(),
-  firstName: z.string().optional(),
-  lastName: z.string().optional(),
   enrollmentNumber: z.string().optional(),
   statusId: numberFilterSchema().optional(),
   email: z.string().optional(),
