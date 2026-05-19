48f14f12c3da998d771be6b0cea6a1dc1dfe9267
flchris <filetelopezchristian@gmail.com>
2026-05-19 12:47:20 -0600

    feat: set up cloudinary service

--- back/src/config/env.ts
+++ back/src/config/env.ts
@@ -8,4 +8,7 @@ export const env = {
   NODE_ENV: process.env.NODE_ENV ?? "development",
   JWT_EXPIRES_IN: Number(process.env.JWT_EXPIRES_IN) || 21600,
   CLIENT_URL: process.env.CLIENT_URL ?? "http://localhost:8080",
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME ?? "",
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY ?? "",
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET ?? "",
 };
