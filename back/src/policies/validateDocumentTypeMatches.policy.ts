c05dc60814b222d5e0971ab970af12b9acef8383
flchris <filetelopezchristian@gmail.com>
2026-06-16 17:10:40 -0600

    refactor: change to a global policy

--- back/src/policies/validateDocumentTypeMatches.policy.ts
+++ back/src/policies/validateDocumentTypeMatches.policy.ts
@@ -1,14 +1,12 @@
 import { HttpError } from "@/errors/http.error";
 import { logger } from "@/config/logger";
 
-export const validateGuardianDocumentTypeCannotChange = (
+export const validateDocumentTypeCannotChange = (
   currentTypeId: number,
   newTypeId: number
 ) => {
   if (currentTypeId !== newTypeId) {
-    logger.warn(
-      `[GUARDIAN-DOCUMENT] No es posible cambiar el tipo de documento`
-    );
+    logger.warn(`[DOCUMENT] No es posible cambiar el tipo de documento`);
 
     throw new HttpError(
       409,
